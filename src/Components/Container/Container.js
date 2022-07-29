69323b91bc754cf74bbb8618a4f4901d141df0bd
arielmell0 <arielmello@rede.ulbra.br>
2022-07-29 02:23:24 -0300

    feat: ajustando container para ocupar 100vw

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     flex-direction: column;
-    width: 100%;
-    height: 90vh;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
 `;
