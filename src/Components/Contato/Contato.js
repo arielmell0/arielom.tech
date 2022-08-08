378570124dd4f73142dfc4fd598ac9ec3e7846b4
arielmell0 <arielmello@rede.ulbra.br>
2022-08-07 21:28:20 -0300

    feat: foto feia adicionada aos contatos

--- src/Components/Contato/Contato.js
+++ src/Components/Contato/Contato.js
@@ -24,20 +24,17 @@ const TextoContato = styled.div`
     font-size: 1.2em;
 `;
 
-const FotoDoFeio = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50vw;
+const FotoDoFeio = styled.img`
+    margin: 2.5% 2.5%;
 
-    background-image: url("../../images/profilePicContact.jpg");
 `;
 
 const Contato = () => {
     return (
         <div id='contato'>
             <ContatoContainer>
-                <FotoDoFeio />
+                <FotoDoFeio src="https://scontent.fccm3-1.fna.fbcdn.net/v/t39.30808-6/289458139_781640299877846_8545634811128899646_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF-20ht10mEXZHrepwwxSvpVHF0ZXzKR81UcXRlfMpHzbqzd8waURDrRxyFAZTDRY5i4g0ODfOQfNERMWoD00cn&_nc_ohc=pXnYGsi_C0MAX-wyK6G&tn=Z_eLk0aLw7oklqaT&_nc_ht=scontent.fccm3-1.fna&oh=00_AT9GQgQL475IH_00eBMSgs-OtBFjSg4UT9l571jpFqGuNg&oe=62F59A77"
+                width="400" height="400" alt="foto do autor"/>
                 <TextoContato>
                     Você pode entrar em contato comigo pelas minhas redes sociais,<br/> fique a vontade
                     para fazer perguntas,<br/> trocar conhecimento ou apenas socializar!
